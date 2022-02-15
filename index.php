7f8947da1453e6e5ef63f9081c3c53ec6991e850
AntonioWilliam <antoniowillia007@hotmail.com>
2022-02-15 11:49:04 -0300

    Primeiro exercicio

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+
+            echo "<h1>Primeiro exercicio</h1>";
+            echo "1 -Atribua dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variávelsoma. A seguir escrever o valor desta variável." ;
+        $a= 5;
+        $b= 6;
+        $soma= $a + $b;
+       
+            echo "<br />" . $a . "+" . $b . "=" . $soma;
+
+        
+        ?>
+    
+</body>
+</html>
